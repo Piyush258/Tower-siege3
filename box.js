379b5990de3b8b9c91562e3c84a267a003e0570b
Piyush258 <66780002+Piyush258@users.noreply.github.com>
2020-10-19 08:47:03 +0530

    Practicing JSON and API calls

---
+++ box.js
@@ -0,0 +1,45 @@
+class Box{
+    constructor(x,y,width,height,c){
+        var options={
+            isStatic: false,
+            restitution: 0.8,
+            density:1.0,
+            friction:0.04
+        }
+        this.body = Bodies.rectangle(x,y,width,height,options);
+        this.width = width;
+        this.height = height;
+        this.colour = c;
+        this.visiblity = 255;
+        World.add(world, this.body);
+    }
+    
+    display(){
+        var pos = this.body.position;
+        var angle = this.body.angle;
+       
+        if (this.body.speed<7){
+            push();
+            translate(pos.x,pos.y)
+            rotate(angle);
+            rectMode(CENTER);
+            stroke("black");
+            strokeWeight(1);
+            fill(this.colour);
+            rect(0,0,this.width,this.height);
+            pop();
+        }
+        else{
+            push();
+            World.remove(world, this.body);
+            this.visiblity = this.visiblity - 5;
+            tint(255,this.visiblity);
+            pop();
+        }
+    }
+    score(){
+        if (this.visiblity<0 && this.visiblity>-105){
+            score++;
+        }
+    }
+};
